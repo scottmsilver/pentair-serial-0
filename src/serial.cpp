e53b51b75ad2e7c9657b4ee6999650b028f8a8e7
Scott Silver <scottmsilver@gmail.com>
2017-07-03 20:44:36 -0700

    Clean up and comment. Delete old code.

--- src/serial.cpp
+++ src/serial.cpp
@@ -1,4 +1,4 @@
-#include <algorithm> 
+#include <algorithm>
 #include <array>
 #include <functional>
 #include <iostream>
@@ -83,7 +83,9 @@ std::string hex(const std::vector<char>& buffer) {
 
 const int kDefaultSerialByteToRead = 128;
 
+// Worker function reads from file and repeatedly writes it to the bassed in buffer.
 class SerialBusFileWorker  {
+private:
   bounded_buffer<char>& mBuffer;
   bool mStopped;
   std::string mFileContents;
@@ -107,7 +109,7 @@ public:
   }
 };
 
-
+// Worker function reads from serial port and writes output to buffer.
 class SerialBusWorker  {
   bounded_buffer<char>& mBuffer;
   bool mStopped;
@@ -135,9 +137,9 @@ public:
   }
 };
 
+// Worker thread reads data from buffer, turns them into pentair_t::message_t's
 class BufferReaderMessageWriter {
   bounded_buffer<char>& mBuffer;
-  
   bool mStopped;
   
 public:
@@ -148,9 +150,8 @@ public:
     bounded_buffer_source source(mBuffer);
     boost::iostreams::stream<bounded_buffer_source> in(source);
     kaitai::kstream stream(&in);
-
+    
     while (!mStopped) {
-      
       pentair_t::message_t message(&stream);
       
       auto command = message.command();
@@ -160,171 +161,10 @@ public:
 };
 
 class MessageReader {
-  
 };
 
-class SerialHandler {
-  int mBytesRead;
-  boost::system::error_code mLastError;
-  
-  SerialHandler(const SerialHandler& that) = delete;
-  
-public:
-  SerialHandler() : mBytesRead(0) {
-  }
-  
-  void read_callback(bool& data_available, boost::asio::deadline_timer& timeout, const boost::system::error_code& error, std::size_t bytes_transferred) {
-    LOG(DEBUG) << "read_callback: e:" << error << " bytes: " << bytes_transferred ;
-    mLastError = error;
-    
-    if (error || !bytes_transferred) {
-      // No data was read!
-      data_available = false;
-    } else {
-      // will cause wait_callback to fire with an error
-      mBytesRead += bytes_transferred;
-      data_available = true;
-      timeout.cancel();
-    }
-  }
-  
-  void wait_callback(boost::asio::serial_port& ser_port, const boost::system::error_code& error) {
-    LOG(DEBUG) << "wait_callback e:" << error;
-    
-    if (error == boost::asio::error::operation_aborted) {
-      // Data was read and this timeout was canceled
-      return;
-    } else {
-      ser_port.cancel();  // will cause read_callback to fire with an error
-    }
-  }
-  
-  int getBytesRead() { return mBytesRead; }
-  boost::system::error_code getLastError() { return mLastError; }
-};
-
-
-class SerialReader {  int mTimeoutMs;
-public:
-  boost::asio::io_service io_svc;
-  boost::asio::serial_port ser_port;
-  
-  SerialReader(int timeoutMs) : mTimeoutMs(timeoutMs), ser_port(io_svc, "/dev/ttyUSB0") {
-    el::Loggers::getLogger("serial");
-    
-    // Set serial port to 9600, N, 1.
-    ser_port.set_option(boost::asio::serial_port_base::baud_rate(9600));
-    ser_port.set_option(boost::asio::serial_port_base::parity(boost::asio::serial_port_base::parity::none));
-    ser_port.set_option(boost::asio::serial_port_base::stop_bits(boost::asio::serial_port_base::stop_bits::one));
-  }
-  
-  void readBlocking(char* wholeBuffer, int bufferSize, int timeoutMs) {
-    CLOG(DEBUG, "serial") << "--> (size = " << bufferSize << ", timeout = " << timeoutMs << ")";
-    
-    boost::asio::read(ser_port, boost::asio::buffer(wholeBuffer, bufferSize));
-    CLOG(DEBUG, "serial") << hex(wholeBuffer, bufferSize);
-    CLOG(DEBUG, "serial") << "<-- readBlocking (size = " << bufferSize << ", timeout = " << timeoutMs << ")";
-  }
-  
-  void readUntil(char x) {
-    CLOG(DEBUG, "serial") << "Looking for " << std::hex << +x;
-    for (;;) {
-      char ch;
-      readBlocking(&ch, 1, 100000); // FIX-ME supposed to be infinity.
-      if (ch == x) {
-        break;
-      }
-    }
-  }
-  
-  char readByte() {
-    char ch = 0;
-    readBlocking(&ch, 1, mTimeoutMs);
-    return ch;
-  }
-  
-  int readShort() {
-    int s16 = 0;
-    
-    // FIX-ME: endianness
-    readBlocking(reinterpret_cast<char*>(&s16) + 1, 1, mTimeoutMs);
-    readBlocking(reinterpret_cast<char*>(&s16), 1, mTimeoutMs);
-    return s16;
-  }
-};
-
-struct RawMessage {
-  char mDestination;
-  char mSource;
-  char mCfiType;
-  std::vector<char> mCfi;
-  
-  RawMessage(char destination, char source, char cfiType, char* cfi, int cfiSize) :
-  mDestination(destination), mSource(source), mCfiType(cfiType), mCfi(cfi, cfi + cfiSize) {
-  }
-  
-  std::string addressType(char address) const {
-    if (address == 0x0f) {
-      return "broad";
-    } else if ((address & 0xf0) == 0x10) {
-      // All 0x1x are main controllers.
-      return "cntrl";
-    } else if ((address & 0xf0) == 0x20) {
-      // All 0x1x are remote controllers.
-      return "remte";
-    } else if ((address & 0xf0) == 0x60) {
-      // All 0x1x are pumps. x are the pump numbers.
-      return "pumpX";
-    } else {
-      return "unkwn";
-    }
-  }
-  
-  void decodeMessage() {
-    switch (mCfiType) {
-      default:
-        LOG(ERROR) << "Decode Failure: " << *this;
-    }
-  }
-};
-
-std::ostream& operator<<(std::ostream& os, RawMessage const& message) {
-  os << message.addressType(message.mSource) << "|" << hex(message.mSource) << "->" << message.addressType(message.mDestination) << "|" << hex(message.mDestination) << " | " << hex(message.mCfiType) << ": " << hex(message.mCfi);
-  return os;
-}
-
-void messageLoop() {
-  SerialReader reader(10000);
-  
-  for (;;) {
-    reader.readUntil(0xa5);
-    LOG(DEBUG) << "Message Begin (0xa5)";
-    auto unknown1 = reader.readByte();
-    LOG(DEBUG) << "Unknown: " << hex(&unknown1, 1);
-    char destinationAddress = reader.readByte();
-    LOG(DEBUG) << "Destination Address: " << hex(&destinationAddress, 1);
-    auto sourceAddress = reader.readByte();
-    LOG(DEBUG) << "Source Address: " << hex(&sourceAddress, 1);
-    auto cfiType = reader.readByte();
-    LOG(DEBUG) << "Command Function Instruction Type: " << hex(&cfiType, 1);
-    auto cfiSize = reader.readByte();
-    LOG(DEBUG) << "Command Function Instruction Size: " << static_cast<int>(cfiSize);
-    char cfi[255];
-    reader.readBlocking(cfi, cfiSize, 5000);
-    LOG(DEBUG) << "Command Function Instruction Data: " << hex(cfi, cfiSize);
-    auto checkSum = reader.readShort();
-    LOG(DEBUG) << "Check Sum: " << checkSum;
-    int calculatedCheckSum = 0xa5 + unknown1 + destinationAddress + sourceAddress + cfiType + cfiSize + std::accumulate(cfi, cfi + cfiSize, 0, std::plus<int>());
-    LOG(DEBUG) << "Calculated sum: " << calculatedCheckSum;
-    
-    if (calculatedCheckSum == checkSum) {
-      RawMessage rawMessage(destinationAddress, sourceAddress, cfiType, cfi, cfiSize);
-      rawMessage.decodeMessage();
-    } else {
-      LOG(ERROR) << "Tossing packet - failed checksum";
-    }
-  }
-}
+//CLOG(DEBUG, "serial") << "--> (size = " << bufferSize << ", timeout = " << timeoutMs << ")";
+//el::Loggers::getLogger("serial");
 
 INITIALIZE_EASYLOGGINGPP
 
@@ -344,11 +184,19 @@ int main(int argc, char* argv[]) {
     }
   }
   
+  if (false) {
+    boost::asio::io_service io_svc;
+    boost::asio::serial_port ser_port(io_svc, "/dev/ttyUSB0");
+    ser_port.set_option(boost::asio::serial_port_base::baud_rate(9600));
+    ser_port.set_option(boost::asio::serial_port_base::parity(boost::asio::serial_port_base::parity::none));
+    ser_port.set_option(boost::asio::serial_port_base::stop_bits(boost::asio::serial_port_base::stop_bits::one));
+  }
+  
   bounded_buffer<char> buffer(2048);
   
   SerialBusFileWorker serialBusFileWorker(buffer, "/Users/ssilver/Google Drive/development/pool/serial/src/serial3.out");
   BufferReaderMessageWriter messageWriter(buffer);
-
+  
   //serialBusFileWorker.foo();
   //messageWriter.foo();
   
