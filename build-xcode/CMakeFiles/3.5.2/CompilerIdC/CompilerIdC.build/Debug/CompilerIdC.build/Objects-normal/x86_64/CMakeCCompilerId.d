ce2762afbd05085bb7b98f39d9a73d8744db1b82
Scott Silver <scottmsilver@gmail.com>
2017-07-03 17:39:38 -0700

    no build-xcode

--- build-xcode/CMakeFiles/3.5.2/CompilerIdC/CompilerIdC.build/Debug/CompilerIdC.build/Objects-normal/x86_64/CMakeCCompilerId.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/ssilver/Google\ Drive/development/pool/serial/build-xcode/CMakeFiles/3.5.2/CompilerIdC/CMakeCCompilerId.c
--- build-xcode/googletest-build/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ssilver/Google Drive/development/pool/serial/build-xcode/googletest-src/googletest
-# Build directory: /Users/ssilver/Google Drive/development/pool/serial/build-xcode/googletest-build/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
